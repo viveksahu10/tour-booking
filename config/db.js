65ab9c65ab5dd16c455f6251227dd64d6fee1a20
Vivek Sahu <viveksahu160305@gmail.com>
2026-05-22 20:37:40 +0530

    use mysql public url

--- config/db.js
+++ config/db.js
@@ -1,20 +1,6 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const pool = mysql.createPool({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT,
-    
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
+const pool = mysql.createPool(process.env.MYSQL_PUBLIC_URL);
 
 module.exports = pool.promise();
